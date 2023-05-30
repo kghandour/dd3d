93669d0570b9b68ae4e9997f332a61c5f20e70bc
Karim ElGhandour <kghandour@live.com>
2023-05-30 16:07:52 +0200

    Skeleton for the datastructure

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import torch
 from classification_model.augmentation import CoordinateTransformation, CoordinateTranslation
 from classification_model.shapepcd_set import ShapeNetPCD, minkowski_collate_fn
-from utils.utils import get_loops
+from utils.utils import get_loops, get_rand_cad
 import configparser
 import os
 import numpy as np
+import torch.nn as nn
+
 
 if __name__ == "__main__":
     print("=======Distillation========")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    outer_loop, inner_loop = get_loops(1) ## Variable defines the number of models per class
     config = configparser.ConfigParser()
     config.read("configs/distillation_config.ini")
     def_conf = config["DEFAULT"]
-    num_classes = 2
+    ipc = def_conf.getint("n_cad_per_class", 1) ## Number of CAD models per class
+    channel = def_conf.getint("channel", 1) ## Should it actually be a channel? 
+    num_classes = def_conf.getint("num_classes", 2)
+    outer_loop, inner_loop = get_loops(ipc) ## Variable defines the number of models per class
     if not os.path.exists(def_conf.get("save_path")):
         os.mkdir(def_conf.get("save_path"))
 
     batch_size = def_conf.getint("batch_size")
     if(def_conf.getboolean("overfit_1")): batch_size = 1
+
+    grid_size = [40, 40, 40]
     
     train_set = ShapeNetPCD(
         data_root=def_conf.get("shapenet_path"),
@@ -41,7 +47,7 @@ if __name__ == "__main__":
         collate_fn=minkowski_collate_fn,
     )
 
-    model_ds, label_ds = ShapeNetPCD(
+    CAD_list, label_list = ShapeNetPCD(
         data_root=def_conf.get("shapenet_path"),
         config=def_conf,
         phase="train"
@@ -49,8 +55,21 @@ if __name__ == "__main__":
    
 
     indices_class = [[] for c in range(2)]
-    for i, lab in enumerate(label_ds):
+    for i, lab in enumerate(label_list):
         indices_class[lab].append(i)
 
-    
+    ## REAL initialization. 
+
+    cad_syn = torch.randn(size=(num_classes*ipc, channel, grid_size[0], grid_size[1], grid_size[2]), requires_grad=True, device=device) ## This is a normal distribution from with mean 0, variance 1. 
+    ## I can also divide by 2 and add 1 so it is from 0 to 1
+    label_syn = torch.tensor([np.ones(ipc)*i for i in range(num_classes)], dtype=torch.long, requires_grad=False, device=device).view(-1)
+
+    print(cad_syn.shape)
+    print('initialize synthetic data from random real pcd')
+    for c in range(num_classes):
+        cad_syn.data[c*ipc:(c+1)*ipc] = get_rand_cad(c, ipc, indices_class, CAD_list).detach().data
+    ''' training '''
+    optimizer_img = torch.optim.SGD([cad_syn, ], lr=def_conf.getfloat("lr_cad", 0.1), momentum=0.5) # optimizer_img for synthetic data
+    optimizer_img.zero_grad()
+    criterion = nn.CrossEntropyLoss().to(device)
     
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import time
 
 def get_loops(ipc):
     # Get the two hyper-parameters of outer-loop and inner-loop.
@@ -20,3 +21,11 @@ def get_loops(ipc):
         outer_loop, inner_loop = 0, 0
         exit('loop hyper-parameters are not defined for %d ipc'%ipc)
     return outer_loop, inner_loop
+
+
+def get_rand_cad(c, n, indices_cls, CAD_list): # get random n images from class c
+    idx_shuffle = np.random.permutation(indices_cls[c])[:n]
+    return CAD_list[idx_shuffle]
+
+def get_time():
+    return str(time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime()))
