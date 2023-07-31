4167324e0d1599e026335ae6de4c63f92eab4be8
Karim ElGhandour <kghandour@live.com>
2023-07-31 23:47:31 +0200

    Added collate

--- mnist_distill.py
+++ mnist_distill.py
@@ -1,10 +1,21 @@
 from configs import settings
 from utils.MnistDataset import MnistDataset
-
+from torch.utils.data import DataLoader
+import utils.MinkowskiCollate as MinkowskiCollate
+import MinkowskiEngine as ME
 
 if __name__ == "__main__":
     settings.init()
 
     outer_loop, inner_loop = 1, 1
-    mnist = MnistDataset()
-    
+    mnist = MnistDataset(phase="train")
+    mnist_train = DataLoader(
+        mnist, num_workers=settings.num_workers, collate_fn=MinkowskiCollate.minkowski_collate_fn, batch_size=settings.batch_size
+    )
+    for i, batch in mnist_train:
+        input = ME.TensorField(
+            coordinates=batch["coordinates"], features=batch["features"]
+        )
+        print("ADD NETWORK HERE")
+        pass
+
---
+++ utils/MinkowskiCollate.py
@@ -0,0 +1,29 @@
+import MinkowskiEngine as ME
+import torch
+
+def minkowski_collate_fn(list_data):
+    coordinates_batch, features_batch, labels_batch = ME.utils.sparse_collate(
+        [d["coordinates"] for d in list_data],
+        [d["features"] for d in list_data],
+        [d["label"] for d in list_data],
+        dtype=torch.float32,
+    )
+    return {
+        "coordinates": coordinates_batch,
+        "features": features_batch,
+        "labels": labels_batch,
+    }
+
+
+def stack_collate_fn(list_data):
+    coordinates_batch, features_batch, labels_batch = (
+        torch.stack([d["coordinates"] for d in list_data]),
+        torch.stack([d["features"] for d in list_data]),
+        torch.cat([d["label"] for d in list_data]),
+    )
+
+    return {
+        "coordinates": coordinates_batch,
+        "features": features_batch,
+        "labels": labels_batch,
+    }
