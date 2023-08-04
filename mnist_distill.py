d2d082a21012c230bc0862cd95fd29ce10e2b395
Karim ElGhandour <kghandour@live.com>
2023-08-04 18:21:16 +0200

    Creating input batch from get_images

--- mnist_distill.py
+++ mnist_distill.py
@@ -1,9 +1,41 @@
 from configs import settings
 from models.MEConv import MEConv, create_input_batch
-from utils.Mnist2D import get_dataset, Mnist2D
+from utils.Mnist2D import Mnist2Dreal, get_dataset, Mnist2D, get_mnist_dataloader
 from torch.utils.data import DataLoader
 from utils.MinkowskiCollate import stack_collate_fn, minkowski_collate_fn
 import MinkowskiEngine as ME
+import os, torch
+import numpy as np
+
+
+def generate_synth(dst_train, num_classes):
+    ''' organize the real dataset '''
+    global indices_class, images_all, labels_all, image_syn, label_syn
+
+    indices_class = [[] for c in range(num_classes)]
+    images_all = [torch.unsqueeze(dst_train[i][0], dim=0) for i in range(len(dst_train))]
+    labels_all = [dst_train[i][1] for i in range(len(dst_train))]
+    for i, lab in enumerate(labels_all):
+        indices_class[lab].append(i)
+    images_all = torch.cat(images_all, dim=0).to(settings.device)
+    labels_all = torch.tensor(labels_all, dtype=torch.long, device=settings.device)
+
+    for c in range(num_classes):
+        print('class c = %d: %d real images'%(c, len(indices_class[c])))
+
+    for ch in range(channel):
+        print('real images channel %d, mean = %.4f, std = %.4f'%(ch, torch.mean(images_all[:, ch]), torch.std(images_all[:, ch])))
+
+    ''' initialize the synthetic data '''
+    image_syn = torch.randn(size=(num_classes*settings.cad_per_class, settings.num_points, 3), dtype=torch.float, requires_grad=True, device=settings.device)
+    label_syn = torch.tensor(np.array([np.ones(settings.cad_per_class)*i for i in range(num_classes)]), dtype=torch.long, requires_grad=False, device=settings.device).view(-1) # [0,0,0, 1,1,1, ..., 9,9,9]
+
+def get_images(c, n): # get random n images from class c
+    idx_shuffle = np.random.permutation(indices_class[c])[:n]
+    img_real = images_all[idx_shuffle]
+    labels = torch.ones((img_real.shape[0],), device=settings.device, dtype=torch.long) * c
+    dataset = Mnist2Dreal(img_real, labels)
+    return get_mnist_dataloader(dataset, n)
 
 if __name__ == "__main__":
     settings.init()
@@ -12,10 +44,21 @@ if __name__ == "__main__":
     channel, im_size, num_classes, class_names, mean, std, dst_train, dst_test, testloader = get_dataset()
     train_loader = DataLoader(dst_train, batch_size=settings.batch_size, shuffle=True, collate_fn=minkowski_collate_fn)
     network = MEConv(in_channel=3, out_channel=10).to(settings.device)
+    total_iterations = settings.distillationconfig.getint("total_iterations")
+    eval_iteration_pool = [total_iterations-1]
+    export_cad_dir = os.path.join(settings.distillationconfig.get("export_dir"), settings.exp_file_name)
+    os.makedirs(export_cad_dir, exist_ok=True)
+
+    generate_synth(dst_train=dst_train, num_classes=10)
+
+    optimizer_img = torch.optim.SGD([image_syn, ], lr=settings.modelconfig.getfloat("dist_lr"), momentum=0.5) # optimizer_img for synthetic data
+    optimizer_img.zero_grad()
     
-    for batch in train_loader:
+
+    for batch in get_images(0, 8):
         input = create_input_batch(batch, True, device=settings.device)
         loss = network(input)
+        
         print(loss)
         
 
--- utils/Mnist2D.py
+++ utils/Mnist2D.py
@@ -2,6 +2,9 @@ from typing import Any, Callable, Optional, Tuple
 from torchvision import datasets, transforms
 import torch
 import configs.settings as settings
+from torch.utils.data import Dataset, DataLoader
+
+from utils.MinkowskiCollate import minkowski_collate_fn
 
 def get_dataset():
     channel = 1
@@ -14,8 +17,8 @@ def get_dataset():
     ### Ignoring transform because I want it to be binary
 
     transform = transforms.Compose([transforms.ToTensor(), ThresholdTransform(thr_255=127)])
-    dst_train = Mnist2D(data_path, train=True, download=True, transform=transform) # no augmentation
-    dst_test = Mnist2D(data_path, train=False, download=True, transform=transform)
+    dst_train = datasets.MNIST(data_path, train=True, download=True, transform=transform) # no augmentation
+    dst_test = datasets.MNIST(data_path, train=False, download=True, transform=transform)
     class_names = [str(c) for c in range(num_classes)]
 
     testloader = torch.utils.data.DataLoader(dst_test, batch_size=256, shuffle=False, num_workers=settings.num_workers)
@@ -24,7 +27,7 @@ class Mnist2D(datasets.MNIST):
     def __init__(self, root: str, train: bool = True, transform = None, target_transform= None, download: bool = False) -> None:
         super().__init__(root, train, transform, target_transform, download)
 
-    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+    def __getitem__(self, index: int):
         image, label = super().__getitem__(index)
         occ_grid = torch.argwhere(image==1)
         return {
@@ -33,6 +36,31 @@ class Mnist2D(datasets.MNIST):
             "label": label
         }
     
+    def __len__(self) -> int:
+       return super().__len__()
+    
+class Mnist2Dreal(Dataset):
+  def __init__(self, images_tensor, labels_tensor):
+    self.images = images_tensor
+    self.labels = labels_tensor
+
+  def __getitem__(self, index):
+    image = self.images[index]
+    label = self.labels[index]
+    occ_grid = torch.argwhere(image==1)
+    return {
+        "coordinates":occ_grid.to(torch.float32),
+        "features":occ_grid.to(torch.float32),
+        "label": int(label)
+    }
+  
+  def __len__(self):
+     return self.images.shape[0]
+  
+def get_mnist_dataloader(dataset, batch_size):
+  train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False, collate_fn=minkowski_collate_fn)
+  return train_loader   
+
 class ThresholdTransform(object): ## https://stackoverflow.com/questions/65979207/applying-a-simple-transformation-to-get-a-binary-image-using-pytorch
   def __init__(self, thr_255):
     self.thr = thr_255 / 255.  # input threshold for [0..255] gray level, convert to [0..1]
