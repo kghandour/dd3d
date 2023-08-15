458b10e23330b005f6655554cf9bbb920d5204dd
Karim ElGhandour <kghandour@live.com>
2023-08-15 23:57:50 +0200

    Clean up

--- models/MEConv.py
+++ models/MEConv.py
@@ -10,22 +10,12 @@ from MinkowskiEngine.MinkowskiOps import (
 class MEConv(ME.MinkowskiNetwork):
     def __init__(self, in_channel, out_channel, embedding_channel=1024, dimension=3, full_minkowski=True):
         ME.MinkowskiNetwork.__init__(self, dimension)
-        self.D = dimension ## 2 for img 3 for 3D
-        # self.mlp = nn.Sequential(
-        #         ME.MinkowskiLinear(3, 128, bias=False),
-        #         ME.MinkowskiReLU(),
-        #     )
-        # self.global_sum_pool = ME.MinkowskiGlobalSumPooling()
-        # self.global_max_pool = ME.MinkowskiGlobalMaxPooling()
-        # self.global_sum = ME.MinkowskiGlobalSumPooling()
+        self.D = dimension
         self.full_minkowski = full_minkowski
         self.features = self._make_layers(3, out_channel, embedding_channel)
         if(not full_minkowski):
             self.classifier = nn.Linear(4500, 10)
-            # self.sparse = MinkowskiToSparseTensor(False)
             self.dense_shape = torch.Size([settings.modelconfig.getint("batch_size"), 1, 5, 30, 30])
-        # self.dense = MinkowskiToDenseTensor(shape=dense_shape)
-        # self.dense = MinkowskiToDenseTensor()
         if(full_minkowski):
             self.global_avg_pool = ME.MinkowskiGlobalAvgPooling()
             self.classifier = ME.MinkowskiLinear(16, 10)
@@ -40,7 +30,6 @@ class MEConv(ME.MinkowskiNetwork):
         out_channel = 64
         for d in range(3):
             layers += [ME.MinkowskiConvolution(in_channel, out_channel, kernel_size=3, dimension=self.D)]
-            ## Ignoring Batching for now
             layers += [ME.MinkowskiInstanceNorm(out_channel)]
             layers += [ME.MinkowskiReLU(inplace=True)]
             layers += [ME.MinkowskiAvgPooling(kernel_size=2, stride=2, dimension= self.D)]
@@ -64,46 +53,10 @@ class MEConv(ME.MinkowskiNetwork):
                 nn.init.constant_(m.bn.bias, 0)
     
     def forward(self, x: ME.TensorField):
-        # out = self.mlp(x)
-        # coordinates = ME.dense_coordinates(x.shape)
-        # out = x.sparse(coordinates=coordinates)
+
         old_shape = x.shape
-        # print(x)
-        # print("Before sparse:", x.shape)
         out = x.sparse()
-        # print("After sparse:", out.shape)
-        # print(out)
-        # print("Sparse features shape", out.features.shape)
-        # print(out.shape)
         out = self.features(out)
-        # out = out.slice(x)
-        # out = out.sparse()
-        # print("After convs and pooling:",out.shape)
-        # dense_temp = out.dense()[0]
-        # print("Dense func",dense_temp.shape)
-        # print("Dense after reshape", dense_temp.view(settings.modelconfig.getint("batch_size"), -1).shape)
-        # print(out.features.shape)
-        # out = self.global_avg_pool(out)
-        # (out, a , b) = out.dense()
-        # print(out.shape, a.shape, b.shape)
-        # out = out.dense()
-        # min_coordinate = out.C.min(0, keepdim=True)
-        # max_coordinate = out.C.max(0, keepdim=True)
-        # print(min_coordinate)
-        # min_coordinate = min_coordinate[:, 1:]
-        # print(settings.modelconfig.getint("batch_size"))
-        # out = out.dense()[0]
-        # out = self.dense(out)
-        # print(out.shape)
-        # # out_dense_reshape = out_dense.view(out_dense.size(0), -1)
-        # out_avg = self.global_avg_pool(out)
-        # out_max = self.global_max_pool(out)
-        # print("Before global avg", out.shape, " After global average", out_avg.shape)
-        # print("Before global max", out.shape, " After global max", out_max.shape)
-        # print(out.shape)
-        # print(out.C.min(0, keepdim=True).values)
-        # print(out.C.max(0, keepdim=True))
-        # print(out.F)
         if(not self.full_minkowski):
             min_coord, _ = out.C.min(0, keepdim=True)
             min_coord = min_coord[:, 1:].cpu()
@@ -114,10 +67,6 @@ class MEConv(ME.MinkowskiNetwork):
         if(self.full_minkowski):
             out = self.global_avg_pool(out)
             out = self.classifier(out).F
-        # print(out.shape)
-        # print(out.shape)
-        # out = self.global_sum(out)
-        # out = self.global_sum_pool(out)
 
         return out
     
